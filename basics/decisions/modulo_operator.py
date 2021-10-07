0d48c420aa001e1a447f8af55e66184fbb5a88d3
Karen Gavilanez Rosero <5gavik85@solent.ac.uk>
2021-10-07 15:55:54 +0100

    added code to demonstrate the use of a modulus operator

---
+++ basics/decisions/modulo_operator.py
@@ -0,0 +1,8 @@
+# Asks user for a whole humber
+print("Please enter a whole number.")
+whole_number = int(input())
+
+if whole_number % 2 == 0:
+    print(f"The number {whole_number} is an even number")
+else:
+    print(f"The number {whole_number} is an odd number")
