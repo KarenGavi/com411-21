e16624c8d01147b0c950a8aa3082f1750c6cccf3
Karen Gavilanez Rosero <5gavik85@solent.ac.uk>
2021-10-07 16:47:40 +0100

    nested decision

---
+++ basics/decisions/nested_decision/nested.py
@@ -0,0 +1,7 @@
+# Ask user to enter the cover type
+print("what type of cover does the book have?")
+book_cover = input()
+
+# decide what cover
+if book_cover == "soft":
+    print("is the book perfect_bound?")
