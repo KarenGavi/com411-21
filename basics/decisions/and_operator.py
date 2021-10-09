8540b05e6a4bf589778e52fd94af3965e97cf511
KarenGavi <5gavik85@solent.ac.uk>
2021-10-09 17:14:44 +0100

    the use of logical and operatore

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,12 @@
+# Ask what they hear
+print("What did I hear?")
+hear = input()
+print("what did I see?")
+see = input()
+
+# Identify the creature
+if (hear == "grr") and (see == "two red eyes"):
+    print("there is a scary creature. I should get out of there!")
+else:
+    print("I am a little scared but i will continue")
+
