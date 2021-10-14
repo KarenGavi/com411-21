3a864967433784a6c4996eae81884eb15535d53d
KarenGavi <5gavik85@solent.ac.uk>
2021-10-14 19:07:26 +0100

    code to demonstrate the use of returm values

--- basics/functions/return_values.py
+++ basics/functions/return_values.py
@@ -4,4 +4,29 @@ def sum_weights(beep_weight, bop_weight):
     return total_weight
 # define second function
 def calc_avg_weight(beep_weight, bop_weight):
-# Call first function
+    avg_weight = sum_weights(beep_weight, bop_weight) /2
+    return avg_weight
+
+# Define third function
+def run():
+    print("What is the weight Beep?")
+    beep_weight = float(input())
+
+    print("What is the weight Bop?")
+    bop_weight = float(input())
+
+    print("What would you like to calculate (sum or average)?")
+    action = input()
+
+    # Determine and carry out action
+    if action == "sum":
+        answer = sum_weights(beep_weight, bop_weight)
+        print(f"the sum of Beep's and Bob's weight is {answer:.2f}")
+    elif action == "average":
+        answer = calc_avg_weight(beep_weight, bop_weight)
+        print(f"the average of Beep's and Bop's weight is {answer:.2f}")
+    else:
+        print("I am not sure what you would like to do.")
+
+# Call to function
+run()
