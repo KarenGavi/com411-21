c2117c9bc92b0bdfcdd835294ac204961dcc19b9
Karen Gavilanez Rosero <5gavik85@solent.ac.uk>
2021-10-14 16:45:25 +0100

    uncomplete task

---
+++ basics/functions/return_values.py
@@ -0,0 +1,7 @@
+# Define first function
+def sum_weights(beep_weight, bop_weight):
+    total_weight = beep_weight + bop_weight
+    return total_weight
+# define second function
+def calc_avg_weight(beep_weight, bop_weight):
+# Call first function
