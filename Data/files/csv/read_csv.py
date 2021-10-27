34f44842c1e7cb8eab39f6131c2522bbbdd7b6cb
KarenGavi <5gavik85@solent.ac.uk>
2021-10-27 14:58:26 +0100

    code to demonstrate how to read a csv file

---
+++ Data/files/csv/read_csv.py
@@ -0,0 +1,22 @@
+# Define first function
+import csv
+def read(file_path):
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+
+        headings = next(csv_reader)
+        print(f"Headings:\n{headings}")
+
+        print("Values:")
+        for values in csv_reader:
+            print(values)
+
+# Define second function
+def run():
+    read("bots.cvs")
+
+if __name__ == "__main__":
+    run()
+
+
+
