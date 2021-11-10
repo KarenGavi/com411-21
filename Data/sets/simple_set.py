51d706f91ab7bece2576b84011b155d49579e77a
KarenGavi <5gavik85@solent.ac.uk>
2021-11-10 15:52:40 +0000

    code to demonstrate a simple set

---
+++ Data/sets/simple_set.py
@@ -0,0 +1,7 @@
+# Define the first function
+def observed():
+    observation = {"Flying Car", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observation
+def run():
+    print(observed())
+run()
