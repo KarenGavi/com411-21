1ce6749aa0673e6af82560003866a9436da919c6
KarenGavi <5gavik85@solent.ac.uk>
2021-11-11 16:52:42 +0000

    code complete

--- visual/plots/squares.py
+++ visual/plots/squares.py
@@ -3,27 +3,24 @@ import matplotlib.pyplot as plt
 def small():
     x = [3,3,4,4,3]
     y = [3,4,4,3,3]
-    plt.plot(x, y, 'r:o')
+    plt.plot(x,y,"r:o" )
 
 # Define second function
 def medium():
-    x = [2,2,5,5,2]
-    y = [2,5,5,2,2]
-    plt.plot(x, y, 'g--s')
+    x = [2,2,5,5]
+    y = [2,5,5,2]
+    plt.plot(x,y,"g--s")
 
-# define third function
+# Define third function
 def large():
-    x = [1,1,6,6,1]
-    y = [1,6,6,1,1]
+    x = [1,1,6,6]
+    y = [1,6,6,1]
+    plt.plot(x,y,"b-p")
 
-    plt.plot(x, y, 'b-p')
-
-# Define last function
+# Define forth function
 def run():
     small()
     medium()
     large()
     plt.show()
-
 run()
-
