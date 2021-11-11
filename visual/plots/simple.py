18b496171cbfc17cbbd91050a37b592e6b8ac4c0
KarenGavi <5gavik85@solent.ac.uk>
2021-11-11 15:26:11 +0000

    code to display a simple line plot

---
+++ visual/plots/simple.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+# Define first function
+
+def display(x , y):
+    plt.plot = (x,y)
+    plt.show()
+
+# Define second function
+def run():
+    x_values = [1,2,3,4,5]
+    y_values = [1,4,9,16,25]
+    display(x_values, y_values)
+
+run()
