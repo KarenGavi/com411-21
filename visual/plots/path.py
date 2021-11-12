7ec3900344478b4f357f6405aeb3b52d4b02f934
KarenGavi <5gavik85@solent.ac.uk>
2021-11-12 22:17:17 +0000

    code to display a path using a line plot

--- visual/plots/path.py
+++ visual/plots/path.py
@@ -1,2 +1,34 @@
 import matplotlib.pyplot as plt
+
 # Define first function
+def coordinate():
+    print("PLease enter an x value:")
+    x = int(input())
+
+    print("Please enter a y value:")
+    y = int(input())
+
+    return (x, y)
+
+# Define second function
+def path():
+    print("Retrieve path...")
+    x_values = []
+    y_values = []
+
+    for count in range(4):
+        data = coordinate()
+        x_values.append(data[0])
+        y_values.append(data[1])
+    return [x_values, y_values]
+
+# Define third function
+def run():
+    values = path()
+    plt.plot(values[0], values[1], "r--o")
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+    plt.show()
+
+
+run()
